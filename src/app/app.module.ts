b31ee99ecdf829138f66d65d3a03d2846f7fd3de
tle-Grantham <65985568+tle-Grantham@users.noreply.github.com>
2020-07-07 18:10:07 -0700

    firebase working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAmOLVEPeMXxVpOGeZg2AaQqx-T9wSHI5A",
+  authDomain: "fintrack-23e43.firebaseapp.com",
+  databaseURL: "https://fintrack-23e43.firebaseio.com",
+  storageBucket: "fintrack-23e43.appspot.com",
+  messagingSenderId: "965260341973"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,17 +9,24 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  logs: Array<any> = [];
-  balance: number;
+  logs: any[];
+  balance: any[];
 
-  constructor(public navCtrl: NavController) {
-    this.logs = [
-      {amount:'400', type:'Entertainment', date: '3/1/2020'},
-      {amount:'360', type:'Food', date: '2/5/2020'}, 
-      {amount:'720', type:'Other', date: '1/23/2020'}, 
-      {amount:'250', type:'Food', date: '1/1/2020'} 
-    ];
-    this.balance = 8270;
+  constructor(public navCtrl: NavController, public af: AngularFireDatabase, ba: AngularFireDatabase) { 
+    af.list('/logs').valueChanges()
+      .subscribe(logs => {this.logs = logs;
+      });
+    //this.logs = [
+      //{amount:'400', type:'Entertainment', date: '3/1/2020'},
+      //{amount:'360', type:'Food', date: '2/5/2020'}, 
+      //{amount:'720', type:'Other', date: '1/23/2020'}, 
+      //{amount:'250', type:'Food', date: '1/1/2020'} 
+    //];
+    //this.balance = 8270;
+    ba.list('/balance').valueChanges()
+    .subscribe(balance => {this.balance = balance;
+    console.log(this.balance);
+    });
   }
 
 }
