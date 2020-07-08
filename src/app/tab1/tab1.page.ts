422e7b94669c210cee6fb66217e8451f1795cd2d
tle-Grantham <65985568+tle-Grantham@users.noreply.github.com>
2020-07-07 18:12:02 -0700

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,13 +16,6 @@ export class Tab1Page {
     af.list('/logs').valueChanges()
       .subscribe(logs => {this.logs = logs;
       });
-    //this.logs = [
-      //{amount:'400', type:'Entertainment', date: '3/1/2020'},
-      //{amount:'360', type:'Food', date: '2/5/2020'}, 
-      //{amount:'720', type:'Other', date: '1/23/2020'}, 
-      //{amount:'250', type:'Food', date: '1/1/2020'} 
-    //];
-    //this.balance = 8270;
     ba.list('/balance').valueChanges()
     .subscribe(balance => {this.balance = balance;
     console.log(this.balance);
